44df0273eaa365d1781cbcf64a7f2b4c39516165
yuzhongrong <956942189@qq.com>
2018-11-29 16:11:27 +0800

    优化结构

--- FoxMan/src/main/java/com/cjwsc/idcm/kotlin/base/KTBaseFragment.kt
+++
@@ -1,6 +0,0 @@
-package foxidcw.android.idcw.common.base
-
-import com.cjwsc.idcm.base.*
-
-abstract class KTBaseFragment<M : BaseModel?,P : BasePresenter<out BaseView<*>, *>?>: BaseFragment<M, P>(){
-}
--- app/src/main/java/com/foxexchange/android/MainActivity.kt
+++ app/src/main/java/com/foxexchange/android/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.foxexchange.android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.cjwsc.idcm.kotlin.base.BaseActivity
+import com.orhanobut.logger.Logger
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    override val layoutId: Int
+        get() = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onInitView(bundle: Bundle?) {
+        Logger.d("-------------onInitView------------>")
     }
+
+    override fun onEvent() {
+        Logger.d("-------------onEvent------------>")
+    }
+
 }
