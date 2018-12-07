5a743d82ce7560a356f73b460ff35e71bdcdad13
Ling <huanglinghui@yjymail.com>
2018-12-07 19:28:53 +0800

    构建pagerapdater公共类，实现SlidingTabLayout切换fragment效果
    Signed-off-by: Ling <huanglinghui@yjymail.com>

--- user/src/main/Java/com.foxexchange.user/MyAssetKTActivity.kt
+++ user/src/main/Java/com.foxexchange.user/MyAssetKTActivity.kt
@@ -2,11 +2,14 @@ package com.foxexchange.user
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.View
 import com.example.user.R
 import com.foxexchange.common.activitys.BaseFoxExChangeKTActivity
+import com.foxexchange.common.adapter.MyPagerAdapter
 import com.foxexchange.user.fragments.AssetListKTFragment
 import com.foxexchange.user.fragments.C2CAssetKTFragment
 import com.foxexchange.user.fragments.CoinAssetKTFragment
+import kotlinx.android.synthetic.main.activity_my_asset_layout.*
 
 /**
  * 我的资产
@@ -17,19 +20,15 @@ class MyAssetKTActivity : BaseFoxExChangeKTActivity(){
     var tabs : Array<String> =
             arrayOf("资产列表", "币币资产", "C2C资产")
 
-    var fragmentList = ArrayList<Fragment>()
-
+    var fragmentList : ArrayList<Fragment> = arrayListOf(
+            AssetListKTFragment(), CoinAssetKTFragment(), C2CAssetKTFragment())
 
     override val layoutId: Int
         get() = R.layout.activity_my_asset_layout
 
     override fun onInitView(bundle: Bundle?) {
-        fragmentList.add(AssetListKTFragment())
-        fragmentList.add(CoinAssetKTFragment())
-        fragmentList.add(C2CAssetKTFragment())
-
-//        frg_tab.
-
+        frag_vp.adapter = MyPagerAdapter(supportFragmentManager, fragmentList)
+        frg_tab.setViewPager(frag_vp, tabs, this, fragmentList)
     }
 
     override fun onEvent() {
