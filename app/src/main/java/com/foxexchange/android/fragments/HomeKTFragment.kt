3ec6f53dd64aa1ce275c6a4c218b71968cf1087d
yuzhongrong <Zhongrong.Yu@lavainternational.com>
2018-12-10 21:34:06 +0800

    优化首页

--- app/src/main/java/com/foxexchange/android/fragments/HomeKTFragment.kt
+++ app/src/main/java/com/foxexchange/android/fragments/HomeKTFragment.kt
@@ -2,15 +2,14 @@ package com.foxexchange.android.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.View
-import android.view.ViewGroup
-import android.widget.RelativeLayout
 import com.cjwsc.idcm.kotlin.base.BaseKTFragment
 import com.flyco.tablayout.SegmentTabLayout
 import com.foxexchange.android.R
+import com.foxexchange.android.beans.HomeDataBean
 import com.foxexchange.android.utils.GlideImageLoader
-import com.lqr.adapter.LQRAdapterForRecyclerView
-import com.lqr.adapter.LQRViewHolderForRecyclerView
+import com.cjwsc.idcm.base.ui.widget.NoScroolListView
+import com.lqr.adapter.LQRAdapterForAbsListView
+import com.lqr.adapter.LQRViewHolderForAbsListView
 import com.youth.banner.Banner
 import com.youth.banner.BannerConfig
 import com.youth.banner.Transformer
@@ -18,43 +17,51 @@ import com.youth.banner.Transformer
 class HomeKTFragment : BaseKTFragment() {
 
 
-    var adapter:LQRAdapterForRecyclerView<*>?=null
+    var adapter:LQRAdapterForAbsListView<Any>?=null
     var titles= mutableListOf<String>("涨幅榜","跌幅榜")
     var fragments:ArrayList<Fragment>?= arrayListOf(IncreaseorDownKTFragment(),IncreaseorDownKTFragment())
+    var homelist: NoScroolListView?=null
 
     override val layoutId: Int
         get() = R.layout.fragment_home_layout
 
     override fun onInitView(bundle: Bundle?) {
-        val root = View.inflate(context, R.layout.layout_home_head, rootView as ViewGroup?) as RelativeLayout
-        adapter=object : LQRAdapterForRecyclerView<Any>(activity,ArrayList<Any>(),R.layout.item_home_list){
+
+        homelist=rootView?.findViewById<NoScroolListView>(R.id.homelist)
 
 
-            override fun convert(helper: LQRViewHolderForRecyclerView?, item: Any?, position: Int) {
-
-
-
+        adapter=object : LQRAdapterForAbsListView<Any>(activity,ArrayList<Any>(),R.layout.item_home_list){
+            override fun convert(helper: LQRViewHolderForAbsListView?, item: Any?, position: Int) {
             }
-
 
         }
 
-        initData(root)
+        initData()
+        homelist?.adapter=adapter
+
+        for(item:Int in 1..9){
+            adapter?.addLastItem(HomeDataBean("1","1","1","1"))
+
+        }
+
+
 
     }
 
 
-    fun initData(root: View){
+    fun initData(){
 
 
 
-       var tab= root.findViewById<SegmentTabLayout>(R.id.segmenttablayout)
+       var tab= rootView?.findViewById<SegmentTabLayout>(R.id.segmenttablayout)
 
-        tab.setTabData(titles.toTypedArray(),activity,
+        tab?.setTabData(titles.toTypedArray(),activity,
                 R.id.tablayoutcontainer,fragments)
 
 
-        initBanner(root,adapter as LQRAdapterForRecyclerView<*>)
+        initBanner()
+
+
 
 
 
@@ -69,37 +76,39 @@ class HomeKTFragment : BaseKTFragment() {
 
     }
 
-    private fun initBanner(root:View,adapter: LQRAdapterForRecyclerView<*>) {
 
-        var banner=root.findViewById<Banner>(R.id.banner)
+
+
+    private fun initBanner() {
+        var banner=rootView?.findViewById<Banner>(R.id.banner)
+
         //设置banner样式
-        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR)
+        banner?.setBannerStyle(BannerConfig.CIRCLE_INDICATOR)
         //设置图片加载器
-        banner.setImageLoader(GlideImageLoader())
+        banner?.setImageLoader(GlideImageLoader())
         //设置图片集合
        // banner.setImages(uris)
         //设置banner动画效果
-        banner.setBannerAnimation(Transformer.Default)
+        banner?.setBannerAnimation(Transformer.Default)
         //设置标题集合（当banner样式有显示title时）
         //
         //  banner.setBannerTitles(titles)
         //设置自动轮播，默认为true
-        banner.isAutoPlay(true)
+        banner?.isAutoPlay(true)
         //设置轮播时间
-        banner.setDelayTime(10000)
+        banner?.setDelayTime(10000)
         //设置指示器位置（当banner模式中有指示器时）
-        banner.setIndicatorGravity(BannerConfig.CENTER)
+        banner?.setIndicatorGravity(BannerConfig.CENTER)
         //banner设置方法全部调用完毕时最后调用
-        banner.start()
+        banner?.start()
 
-        banner.setOnBannerListener {
+        banner?.setOnBannerListener {
 //            val intent = Intent(activity, WebViewActivity::class.java)
 //            intent.putExtra("url", "https://cmccoin.io/")
 //
 //            startActivity(intent)
         }
 
-        adapter.headerAndFooterAdapter.addHeaderView(root)
 
 
     }
--- app/src/main/java/com/foxexchange/android/fragments/IncreaseorDownKTFragment.kt
+++ app/src/main/java/com/foxexchange/android/fragments/IncreaseorDownKTFragment.kt
@@ -23,38 +23,12 @@ class IncreaseorDownKTFragment :BaseKTFragment() {
 
 
     override fun onInitView(bundle: Bundle?) {
-        lqrRecyclerView=rootView?.findViewById(R.id.datalist)
-        lqrRecyclerView?.adapter=object:LQRAdapterForRecyclerView<IncreaseDownBean>(activity,
-                ArrayList<IncreaseDownBean>(),R.layout.item_inpreasedown) {
-            override fun convert(helper: LQRViewHolderForRecyclerView, item: IncreaseDownBean, position: Int) {
-
-                var tr_tip= helper.getView<TextView>(R.id.tr_tip)//交易兑
-                var tr_price_tip= helper.getView<TextView>(R.id.tr_price_tip)//24h成交额
-                var rate= helper.getView<TextView>(R.id.rate)//汇率
-                var rmb= helper.getView<TextView>(R.id.rmb)//价值多少rmb
 
 
-
-
-
-            }
-
-        }
-
-
-        initData()
 
     }
 
 
-    fun initData(){
-
-        adapter= lqrRecyclerView?.adapter as LQRAdapterForRecyclerView<Any>
-
-        for (item:Int in 0..5)adapter?.addLastItem(IncreaseDownBean("1","2","3","4"))
-
-
-    }
 
     override fun onEvent() {
 
